456db0796f6f5247e7eab2afffbe1091c81079b8
Denzel Pressey <pressey90@gmail.com>
2023-11-12 12:14:38 -0500

    added other config files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: 'path.join(__dirname, "public")',
+        port: 8080,
+        watchFiles: ["src/**/*.tsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    entry: 'src/App.ts',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+};
