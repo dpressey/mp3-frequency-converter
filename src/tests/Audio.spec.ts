d493a765fac8472b14e9bfc7e9fa0d8caa595482
Denzel Pressey <pressey90@gmail.com>
2023-11-15 14:23:38 -0500

    renamed this test file

---
+++ src/tests/Audio.spec.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import Audio from '../Audio';
+
+const mockAudio: React.FC = Audio;
+
+// TODO: test for Audio component existing on the page
+
+// TODO: test for the Deezer API call
+
+// TODO: test for getFrequencyData()
+
+// test for radioValue state update
+test('radio button value changed', function(){
+    // TODO: 
+        // import audio component (DONE)
+        // mock setradioValue()
+
+});
