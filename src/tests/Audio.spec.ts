191bf38ff93f273693b8fb6df77e79f127e1aaa7
Denzel Pressey <pressey90@gmail.com>
2023-11-22 19:12:07 -0500

    imported enzyme for tests

--- src/tests/Audio.spec.ts
+++ src/tests/Audio.spec.ts
@@ -1,21 +1,20 @@
 import React from 'react';
 import Audio from '../Audio';
-
-const mockAudio: React.FC = Audio;
-
-// TODO: test for Audio component existing on the page
-
-// TODO: test for the Deezer API call
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new Adapter() });
 
 // TODO: test for getFrequencyData()
 
 // test for radioValue state update
 test('radio button value changed', function(){
-    // TODO: 
-        // import audio component (DONE)
-        // mock setradioValue()
-        const radioValue = "440";
-        const setRadioValue = jest.fn();
-        const useStateSpy = jest.spyOn(React, "useState");
-        useStateSpy.mockImplementation((radioValue: string) => {[radioValue, setRadioValue]})
+    const radioValue = "440";
+    const newRadioValue = "432";
+
+    const setRadioValue = jest.fn();
+    const useStateSpy = jest.spyOn(React, 'useState');
+    useStateSpy.mockImplementation(radioValue => [radioValue, setRadioValue]);
+
+    // TODO: import react-testing-library (DONE)
+    // TODO: write assertion
 });
